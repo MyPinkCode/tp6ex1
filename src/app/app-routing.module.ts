763315465f0a6d447a7a38466fffbdc5ca782604
yassine7319 <yassine.ne_7319@outlook.com>
2019-11-20 10:03:12 +0100

    debut tp6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProduitsComponent } from './application/produits/produits.component';
+import { AccueilComponent } from './application/accueil/accueil.component';
+import { ContactComponent } from './application/contact/contact.component';
+import { ErrorComponent } from './application/error/error.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{path:'accueil',component:AccueilComponent},
+                        {path:'produits',component:ProduitsComponent},
+                        {path:'contact',component:ContactComponent},
+                        {path:'',redirectTo:'accueil',pathMatch:'full'},
+                        {path:'**',component:ErrorComponent}  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitsComponent } from './application/produits/produits.component';
+import { AccueilComponent } from './application/accueil/accueil.component';
+import { ContactComponent } from './application/contact/contact.component';
+import { ErrorComponent } from './application/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitsComponent,
+    AccueilComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
